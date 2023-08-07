4f4debb5cca5adf2384e708803ff5efb07504586
TOWHIDUR RAHMAN ASIF <towhidasif@gmail.com>
2023-08-07 18:43:28 +0600

    Movement of bike

--- src/RacingGame.java
+++ src/RacingGame.java
@@ -8,30 +8,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class RacingGame extends JFrame implements ActionListener, KeyListener {
-    private static final int WIDTH = 600;
-    private static final int HEIGHT = 400;
-    private static final int BIKE_WIDTH = 20;
-    private static final int BIKE_HEIGHT = 40;
+public class RacingGame extends JFrame implements  ActionListener,KeyListener {
+    public static final int HEIGHT=400;
+    public static final int WIDTH=600;
+    public static final int BIKE_HEIGHT=40;
+    private static final int CAR_WIDTH = 30;
+    private static final int CAR_HEIGHT = 60;
+    private static final int GAME_SPEED = 10;
+    public static final int BIKE_WIDTH=20;
+    private static final int BIKE_SPEED = 4;
+    private static final int CAR_SPEED = 2;
+    private static final int SPEED_INCREMENT = 1;
+    public static final int INITIAL_DELAY=1000;
 
-    private static final int GIFT_BOX_WIDTH = 30;
-    private static final int GIFT_BOX_HEIGHT = 30;
-    private static final int GAME_SPEED = 10; // Milliseconds
-    private static final int BIKE_SPEED = 4;  // Bike's horizontal movement speed
-    private static final int CAR_SPEED = 2;  // Car's vertical movement speed
-
-    private static final int INITIAL_DELAY = 1000; // Initial delay without other vehicles (in milliseconds)
 
     private int bikeX;
+    private int carX;
 
-    private int giftBoxX;
-
-    private int score=0;
     private int carSpeed;
     private int timeElapsed;
-
-
-    private List<Point> gift;
+    private List<Point> cars;
     private Random random;
 
     public RacingGame() {
@@ -41,15 +37,11 @@ public class RacingGame extends JFrame implements ActionListener, KeyListener {
         setLocationRelativeTo(null);
 
         bikeX = WIDTH / 2 - BIKE_WIDTH / 2; // Bike starts at the center of the screen
-
-        giftBoxX = WIDTH/2 - GIFT_BOX_WIDTH/2; // Gift box starts off the screen initially
-        //giftBoxY = -GIFT_BOX_HEIGHT;
-        score = 0;
+        carX = -CAR_WIDTH; // Car starts off the screen initially
         carSpeed = CAR_SPEED;
         timeElapsed = 0;
 
-
-        gift = new ArrayList<>();
+        cars = new ArrayList<>();
         random = new Random();
 
         Timer timer = new Timer(GAME_SPEED, this);
@@ -59,27 +51,24 @@ public class RacingGame extends JFrame implements ActionListener, KeyListener {
         setFocusable(true);   // Ensure the frame is focused to receive key events
     }
 
+
     @Override
     public void actionPerformed(ActionEvent e) {
         moveElements();
-        checkCollisions();
         repaint();
     }
 
     private void moveElements() {
-
         if (timeElapsed >= INITIAL_DELAY && timeElapsed % 1000 == 0) {
-            int randX = random.nextInt(WIDTH - GIFT_BOX_WIDTH);
-            gift.add(new Point(randX, -GIFT_BOX_HEIGHT));
+            int randX = random.nextInt(WIDTH - CAR_WIDTH);
+            cars.add(new Point(randX, -CAR_HEIGHT));
         }
 
-
-
-        for (int i = 0; i < gift.size(); i++) {
-            Point gif = gift.get(i);
-            gif.y += carSpeed;
-            if (gif.y >= HEIGHT ) {
-                gift.remove(i);
+        for (int i = 0; i < cars.size(); i++) {
+            Point car = cars.get(i);
+            car.y += carSpeed;
+            if (car.y >= HEIGHT) {
+                cars.remove(i);
                 i--;
             }
         }
@@ -87,37 +76,36 @@ public class RacingGame extends JFrame implements ActionListener, KeyListener {
 
         timeElapsed += GAME_SPEED; // Track the time elapsed
     }
-
     private void checkCollisions() {
         Rectangle bikeRect = new Rectangle(bikeX, HEIGHT - BIKE_HEIGHT, BIKE_WIDTH, BIKE_HEIGHT);
         //Point giftBoxCenter = new Point(giftBoxX + GIFT_BOX_WIDTH / 2, giftBoxY + GIFT_BOX_HEIGHT / 2);
 
-        for (Point gf : gift) {
-            Rectangle gifRect = new Rectangle(gf.x, gf.y, GIFT_BOX_WIDTH, GIFT_BOX_HEIGHT);
+        for (Point car : cars) {
+            Rectangle carRect = new Rectangle(car.x, car.y, CAR_WIDTH, CAR_HEIGHT);
 
-            if (bikeRect.intersects(gifRect)) {
-                score++;
-                gift.remove(gf);
-            }
+
         }
     }
 
-    @Override
-    public void paint(Graphics g) {
+
+
+    public void paint(Graphics g){
         super.paint(g);
 
         g.setColor(Color.BLUE);
         g.fillRect(bikeX, HEIGHT - BIKE_HEIGHT, BIKE_WIDTH, BIKE_HEIGHT);
 
-
-        g.setColor(Color.GREEN);
-        for (Point gif : gift) {
-            g.fillRect(gif.x, gif.y, GIFT_BOX_WIDTH, GIFT_BOX_HEIGHT);
+        g.setColor(Color.RED);
+        for (Point car : cars) {
+            g.fillRect(car.x, car.y, CAR_WIDTH, CAR_HEIGHT);
         }
-
     }
 
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
@@ -130,12 +118,7 @@ public class RacingGame extends JFrame implements ActionListener, KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
-        // Implement any required logic when a key is released
-    }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // Implement any required logic when a key is typed
     }
 
     public static void main(String[] args) {
